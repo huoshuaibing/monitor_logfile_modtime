5df3db3dbedbd3f882e248bdf6a8f19b671bbd95
huoshuaibing <1115284873@qq.com>
2019-06-14 14:25:31 +0800

    add a new file

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+import (
+	"os"
+	"time"
+	"log"
+	"github.com/royeo/dingrobot"
+)
+func GetFileModTime(path string) int64 {
+	f,err := os.Open(path)
+	if err != nil{
+		println("open file error")
+		return time.Now().Unix()
+	}
+	defer f.Close()
+	fi,err := f.Stat()
+	if err != nil{
+		println("stat fileinfo err")
+		return time.Now().Unix()
+	}
+	return fi.ModTime().Unix()
+}
+func send2dingtalk(){
+	webhook := "https://oapi.dingtalk.com/robot/send?access_token=598f161d26735e2a38c314815de809b708534ab31d1c2ff7c7abbe84a8e0243c"
+	robot := dingrobot.NewRobot(webhook)
+	content := "xxx服务已停止写日志"
+	atMobiles := []string{"17600147211"}
+	isAtAll := false
+	err := robot.SendText(content,atMobiles,isAtAll)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func main() {
+	loopWorker()
+}
+
+func loopWorker() {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			t1 := GetFileModTime("/Users/edz/Desktop/1.py")
+			now := time.Now()
+			timestamp1 := now.Unix()
+			if timestamp1 - t1 > 1800 {
+				send2dingtalk()
+			}else{
+				return
+			}
+		}
+	}
+}
